cb790f8edc86d6dc85b876546f2291a23f7ca171
Luís Bevilacqua <luisfernandobpv@gmail.com>
2017-07-20 22:06:47 -0300

    Dynamically generate table

---
+++ ant.js
@@ -0,0 +1,13 @@
+$(() => {
+  generateTable(10, 10);
+})
+
+function generateTable(x, y) {
+  $('body').append('<table></table>')
+  for(i = 0; i < x; i++){
+    $('table').append('<tr class=table-row-' + i + '></tr>')
+    for(j = 0; j < x; j++){
+      $('.table-row-' + i).append('<td class="table-cell-' + i + '-' + j + ' light"></td>')
+    }
+  }
+}
