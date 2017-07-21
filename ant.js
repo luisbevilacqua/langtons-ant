04722220150415ec7becd37216052196771aa12b
Luís Bevilacqua <luisfernandobpv@gmail.com>
2017-07-20 22:09:52 -0300

    Using interpolation to make code more readable

--- ant.js
+++ ant.js
@@ -5,9 +5,9 @@ $(() => {
 function generateTable(x, y) {
   $('body').append('<table></table>')
   for(i = 0; i < x; i++){
-    $('table').append('<tr class=table-row-' + i + '></tr>')
+    $('table').append(`<tr class=table-row-${i}></tr>`)
     for(j = 0; j < x; j++){
-      $('.table-row-' + i).append('<td class="table-cell-' + i + '-' + j + ' light"></td>')
+      $(`.table-row-${i}`).append(`<td class="table-cell-${i}-${j} light"></td>`)
     }
   }
 }
