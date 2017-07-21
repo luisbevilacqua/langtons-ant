1eac4c36fd3f2fc4be50d8ca99ef431f8dc84121
Luís Bevilacqua <luisfernandobpv@gmail.com>
2017-07-20 22:36:58 -0300

    Created ant class

--- ant.js
+++ ant.js
@@ -1,5 +1,6 @@
 $(() => {
   generateTable(10, 10);
+  ant = new Ant(5,5);
 })
 
 function generateTable(x, y) {
@@ -9,5 +10,21 @@ function generateTable(x, y) {
     for(j = 0; j < x; j++){
       $(`.table-row-${i}`).append(`<td class="table-cell-${i}-${j} light"></td>`)
     }
+  }
+}
+
+class Ant {
+  // ALLOWED_DIRECTIONS = ["up", "right", "down", "left"]
+
+  constructor(x = 0, y = 0, direction = "up"){
+    this.positionX = x;
+    this.positionY = y;
+    this.direction = direction;
+    this.printAnt();
+  }
+
+  printAnt() {
+    console.log(`table-cell-${this.positionX}-${this.positionY}`)
+    $(`.table-cell-${this.positionX}-${this.positionY}`).addClass('ant')
   }
 }
